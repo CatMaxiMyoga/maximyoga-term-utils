3d2cd6c10e19398ae461efa6ee60563769226265
Maxi Myoga <l.kupsch1407.new+github@gmail.com>
2025-07-29 20:57:03 +0200

    Added linting rules and made file contents comply

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+#![deny(missing_docs)]
+#![deny(clippy::panic)]
+#![deny(unused_must_use)]
+#![deny(clippy::expect_used)]
+#![deny(clippy::unwrap_used)]
+#![deny(unused_crate_dependencies)]
+
+//! Provides multiple terminal utilities.
+
+/// Temporary Placeholder Function to simulate functionality
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
